f522ad646867ba38d598ad943da428f499e74a6b
som2303 <72679335+som2303@users.noreply.github.com>
2025-02-28 15:36:14 +0900

    [Bronze II] Title: 다이얼, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+a=input()
+ans=0
+for i in a:
+  if i in 'ABC':
+    ans+=3
+  elif i in 'DEF':
+    ans+=4
+  elif i in 'GHI':
+    ans+=5
+  elif i in 'JKL':
+    ans+=6
+  elif i in 'MNO':
+    ans+=7
+  elif i in 'PQRS':
+    ans+=8
+  elif i in 'TUV':
+    ans+=9
+  elif i in 'WXYZ':
+    ans+=10
+  else:
+    ans+=11
+print(ans)
